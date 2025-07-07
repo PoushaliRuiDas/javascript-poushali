78a1a6111961694afdbdb2ff9d65c4692b2eb9ac
PoushaliRuiDas <121057603+PoushaliRuiDas@users.noreply.github.com>
2025-07-07 17:20:04 +0000

    conversion

---
+++ first.js
@@ -0,0 +1,118 @@
+/* let a=28;
+let b=5;
+console.log("a = ",a, "& b = ",b);
+console.log("a + b = ",a + b);
+console.log("a - b = ",a - b);
+console.log("a * b = ",a * b);
+console.log("a / b = ",a / b);
+console.log("a % b = ",a % b);
+console.log("a ** b = ",a ** b); // 28^5 */
+
+// Unary operator
+
+/* let a=28;
+let b=5;
+console.log("a = ",a, "& b = ",b);
+//a = a + 1;
+//console.log(a);
+a++;
+console.log("a++ = ",a++);
+console.log(a);
+//b = b - 1;
+//console.log(b);
+b--;
+console.log("b-- = ",b--); 
+console.log(b); */
+
+// Assignment operator
+// let a=28;
+// let b=5;
+
+// a += 4;  //a = a + 4
+// console.log("a = ",a);  //32
+
+// a -= 4;
+// console.log("a = ",a); //24 
+
+// a *= 4;
+// console.log("a = ",a); //112
+
+// a /= 4;
+// console.log("a = ",a); //7
+
+// a %= 4;
+// console.log("a = ",a); //0
+
+// a **= 4;
+// console.log("a = ",a); //28^4 = 614656
+
+// Comparison Operator
+/* let a=28;
+let b=5;
+
+console.log("a == b ",a == b); //true or false [!=, ===, !==, >, >=, <, <=] */
+
+// Logical Operators
+/* let a=28;
+ let b=5;
+console.log("cond1 && cond2", a < b && a === 28);  //[true/False]
+ console.log("cond1 || cond2", a < b || a === 28);  //[true/False]
+console.log("!cond1", !(a < b));  //[true/False] */
+
+// Conditional Statement
+/* let age = 20;
+
+if (age >= 21) {
+    console.log("You are eligible to drink");
+}   
+if (age < 21) {
+    console.log("You are not eligible to drink");
+}     // we can use multiple if statement
+*/
+
+/* let mode = "light";
+let color;
+
+if (mode === "dark"){
+    color = "black";
+}
+
+else if (mode === "light"){
+    color = "white";
+}
+console.log(color);  */
+
+// ODD or EVEN 
+/* let num = 20;
+
+if (num % 2 === 0) {
+    console.log(num, "is Even");
+}
+else {
+    console.log(num, "is Odd");
+} */
+
+/* let mode = "dark";
+let color;
+
+if (mode === "dark"){
+    color = "black";
+} else if (mode === red){
+    color = "red";
+} else {
+    color = "white";
+}
+console.log(color);  // output: black
+*/
+// if (mode === "dark") console.log(mode); //Wrong for good practices
+
+// Ternary Operator
+/* let age = 20;
+let message = (age >= 21) ? "You are eligible to drink" : "You are not eligible to drink"; // condition ? true : false
+
+console.log(message);  // output: You are eligible to drink
+*/
+// for loop
+ for( let count=1;count<=5;count++) {
+    console.log("Poushali")
+ }
