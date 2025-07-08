b06e7dd691340814cdf0b68ed85cd1d59b89001b
PoushaliRuiDas <121057603+PoushaliRuiDas@users.noreply.github.com>
2025-07-08 06:19:40 +0000

    operations

--- learn.js
+++ learn.js
@@ -1,9 +1,31 @@
 // datatypes
+//number conversion
 //let score = "33"
-let score = null
+/*let score = null
 console.log( typeof score);
 console.log(typeof(score));
 
 let valueInNumber = Number(score)
 console.log( typeof valueInNumber);
-console.log (valueInNumber);
+console.log (valueInNumber); */
+//boolean conversion
+// string conversin
+
+
+                       //operations
+
+let value = 3
+let negValue = -value
+//console.log (negValue);    
+// arithmetic operations
+
+//***************************operation */
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
+
+// === (strict check)
+// avoid this type of code always do clean code
